334c66d597d2e9c35fc4c83a7535aa30af6668f2
FedeBrus <federico.brusadin@gmail.com>
2026-05-13 21:38:36 +0200

    fixes to database schema

--- src/main/resources/defaults.sql
+++ src/main/resources/defaults.sql
@@ -1,39 +1,22 @@
-CREATE TABLE IF NOT EXISTS Programs(
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    name            VARCHAR(255) NOT NULL,
-    is_default      BOOLEAN NOT NULL,
-    owner           VARCHAR(255)
-);
+INSERT INTO Programs (name, is_default, owner) VALUES ('Cardio', TRUE, 'DEFAULT');
+INSERT INTO Programs (name, is_default, owner) VALUES ('Strength', TRUE, 'DEFAULT');
+INSERT INTO Programs (name, is_default, owner) VALUES ('Full Body', TRUE, 'DEFAULT');
+INSERT INTO Programs (name, is_default, owner) VALUES ('Push/Pull/Legs', TRUE, 'DEFAULT');
 
-CREATE TABLE IF NOT EXISTS Exercises(
-    name            VARCHAR(255) NOT NULL,
-    sets            INT NOT NULL,
-    reps            INT NOT NULL,
-    kcal            INT NOT NULL,
-    program_id      INT NOT NULL,
-    PRIMARY KEY (name, program_id),
-    FOREIGN KEY (program_id) REFERENCES Programs(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+INSERT INTO Exercises (name, sets, reps, kcal, program_name, program_owner) VALUES
+('Salto della corda', 5, 100, 150, 'Cardio', 'DEFAULT'),
+('Burpees', 4, 15, 80,  'Cardio', 'DEFAULT');
 
-INSERT INTO Programs (name, is_default, owner) VALUES ('Cardio', TRUE, NULL);
-INSERT INTO Programs (name, is_default, owner) VALUES ('Strength', TRUE, NULL);
-INSERT INTO Programs (name, is_default, owner) VALUES ('Full Body', TRUE, NULL);
-INSERT INTO Programs (name, is_default, owner) VALUES ('Push/Pull/Legs', TRUE, NULL);
+INSERT INTO Exercises (name, sets, reps, kcal, program_name, program_owner) VALUES
+('Panca Piana', 5, 5, 100, 'Strength', 'DEFAULT'),
+('Stacco da terra', 4, 6, 120, 'Strength', 'DEFAULT'),
+('Military Press', 4, 8, 90, 'Strength', 'DEFAULT');
 
-INSERT INTO Exercises (name, sets, reps, kcal, program_id) VALUES
-('Salto della corda', 5, 100, 150, 1),
-('Burpees', 4, 15, 80, 1);
+INSERT INTO Exercises (name, sets, reps, kcal, program_name, program_owner) VALUES
+('Squat', 4, 10, 150, 'Full Body', 'DEFAULT'),
+('Trazioni alla sbarra', 3, 8, 70, 'Full Body', 'DEFAULT'),
+('Affondi', 3, 12, 100, 'Full Body', 'DEFAULT');
 
-INSERT INTO Exercises (name, sets, reps, kcal, program_id) VALUES
-('Panca Piana', 5, 5, 100, 2),
-('Stacco da terra', 4, 6, 120, 2),
-('Military Press', 4, 8, 90, 2);
-
-INSERT INTO Exercises (name, sets, reps, kcal, program_id) VALUES
-('Squat', 4, 10, 150, 3),
-('Trazioni alla sbarra', 3, 8, 70, 3),
-('Affondi', 3, 12, 100, 3);
-
-INSERT INTO Exercises (name, sets, reps, kcal, program_id) VALUES
-('Dips', 3, 12, 60, 4),
-('Leg Press', 3, 15, 130, 4);
+INSERT INTO Exercises (name, sets, reps, kcal, program_name, program_owner) VALUES
+('Dips', 3, 12, 60, 'Push/Pull/Legs', 'DEFAULT'),
+('Leg Press', 3, 15, 130, 'Push/Pull/Legs', 'DEFAULT');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Programs(
-    id              INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     is_default      BOOLEAN NOT NULL,
-    owner           VARCHAR(255)
+    owner           VARCHAR(255),
+    PRIMARY KEY (name, owner)
 );
 
 CREATE TABLE IF NOT EXISTS Exercises(
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS Exercises(
     sets            INT NOT NULL,
     reps            INT NOT NULL,
     kcal            INT NOT NULL,
-    program_id      INT NOT NULL,
-    PRIMARY KEY (name, program_id),
-    FOREIGN KEY (program_id) REFERENCES Programs(id) ON UPDATE CASCADE ON DELETE CASCADE
+    program_name    VARCHAR(255),
+    program_owner   VARCHAR(255),
+    PRIMARY KEY (name, program_name, program_owner),
+    FOREIGN KEY (program_name, program_owner) REFERENCES Programs(name, owner) ON UPDATE CASCADE ON DELETE CASCADE
 );
